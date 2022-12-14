8afda085511bea73162a5280d6ea15e95269877c
Mark-dev30 <fronterremarco@gmail.com>
2022-12-14 18:01:54 +0100

    Completed Milestone 1 and Milestone 2

--- js/script.js
+++ js/script.js
@@ -6,23 +6,23 @@ const items = [
     },
     {
         image: "./img/02.webp",
-        title: "ciccio",
-        description: "Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man."
+        title: "Ratchet and Clank",
+        description: "Ratchet & Clank is a series of platform-adventure video games developed by Insomniac Games and published by Sony Computer Entertainment."
     },
     {
         image: "./img/03.webp",
-        title: "Marvel's Spiderman Miles Morale",
-        description: "Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man."
+        title: "Fortnite",
+        description: "Fortnite is a 2017 third-person shooter video game developed by People Can Fly and published by Epic Games for consoles and PC."
     },
     {
         image: "./img/04.webp",
-        title: "Marvel's Spiderman Miles Morale",
-        description: "Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man."
+        title: "Stray",
+        description: "Stray is a 2022 dynamic adventure video game developed by BlueTwelve Studio and published by Annapurna Interactive for PlayStation 4, PlayStation 5 and Microsoft Windows."
     },
     {
         image: "./img/05.webp",
-        title: "Marvel's Spiderman Miles Morale",
-        description: "Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man."
+        title: "Marvel's Avengers",
+        description: "Marvel's Avengers is an action-adventure video game developed by Crystal Dynamics and Eidos Montréal, and published by Square Enix."
     }
 ];
 
@@ -45,16 +45,39 @@ function createElements(){
 
 createElements();
 
-const single_item = document.getElementsByClassName('items');
+const single_item = document.getElementsByClassName('items'); //Si trasforma in un array
 let itemActive = 0;
 single_item[itemActive].classList.add('active');
 
 const next = document.querySelector(".btn-next")
 const prev = document.querySelector(".btn-prev")
 
+const circle = document.getElementsByClassName('circle');
+console.log(circle)
+circle[itemActive].classList.add('active');
+
+
+
 next.addEventListener('click', function(){
 
     single_item[itemActive].classList.remove('active');
+    circle[itemActive].classList.remove('active');
+    if(itemActive == 4){
+        itemActive = -1;
+    }
     itemActive++
     single_item[itemActive].classList.add('active');
-})
+    circle[itemActive].classList.add('active');
+});
+
+prev.addEventListener('click', function(){
+
+    single_item[itemActive].classList.remove('active');
+    circle[itemActive].classList.remove('active');
+    if(itemActive == 0){
+        itemActive = 5;
+    }
+    itemActive--
+    single_item[itemActive].classList.add('active');
+    circle[itemActive].classList.add('active');
+});
